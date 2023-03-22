ae704e41ef63eb601f6d56587cc6f6543227a654
GutierrezJose <gutierreztorrezjose@gmail.com>
2023-03-22 15:43:14 -0400

    Finished show quote of breaking bad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Frase } from './components/Frase'
+import styled from '@emotion/styled';
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: 'Arial', Arial, Helvetica, sans-serif;
+  color: #FFFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
 function App() {
-  return (
-    <h1>Breaking bad</h1>
+
+  const [ frase, guardarFrase ] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  }
+
+  return ( 
+    <Contenedor>
+      <Frase 
+        frase={frase}
+      />
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
---
+++ src/components/Frase.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Frase = ({frase}) => {
+    return (
+        <div>
+            <h1>{frase.quote}</h1>
+            <p>{frase.author}</p>
+        </div>
+    )
+}
